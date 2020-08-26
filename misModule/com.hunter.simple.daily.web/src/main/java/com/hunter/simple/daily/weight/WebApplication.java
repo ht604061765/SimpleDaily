4c334a40db6f939c1bd3373866d5f3c367d91442
hantao <15943286270@163.com>
2020-08-26 23:23:18 +0800

    node.js初体验

---
+++ misModule/com.hunter.simple.daily.web/src/main/java/com/hunter/simple/daily/weight/WebApplication.java
@@ -0,0 +1,21 @@
+package com.hunter.simple.daily.weight;
+
+import com.hunter.base.common.utils.StringUtils;
+import com.hunter.base.common.vo.RemoveBlankTypeEnum;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"com.hunter"})
+@ServletComponentScan
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+        String aaa = StringUtils.removeBlank("   asdg   ", RemoveBlankTypeEnum.ALL);
+        System.out.println("======WebApplication启动完成======");
+    }
+
+}

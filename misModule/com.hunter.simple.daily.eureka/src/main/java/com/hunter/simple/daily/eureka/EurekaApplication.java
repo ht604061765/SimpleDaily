2c25e8e19102bf46f0baa52c3de99f0d3ba29af5
hantao <15943286270@163.com>
2020-10-04 15:25:08 +0800

    eureka server 搭建完

---
+++ misModule/com.hunter.simple.daily.eureka/src/main/java/com/hunter/simple/daily/eureka/EurekaApplication.java
@@ -0,0 +1,29 @@
+package com.hunter.simple.daily.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+        System.out.println("======EurekaApplication 应用启动完成======");
+    }
+
+    @EnableWebSecurity
+    public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+            super.configure(http);
+        }
+    }
+
+}

8437f5da5150e556b1e262884c132f544d2af19f
Hunter <15943286270@163.com>
2020-10-08 18:06:24 +0800

    新增zuul网关服务

---
+++ misModule/com.hunter.simple.daily.zuul/src/main/java/com/hunter/simple/daily/zuul/ZuulApplication.java
@@ -0,0 +1,22 @@
+package com.hunter.simple.daily.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"com.hunter"})
+@ServletComponentScan
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+        System.out.println("======Zuul 应用启动完成======");
+    }
+
+}

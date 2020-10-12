e9e99d330a9bf561b3b4f78ab3183fdcfaf4d4be
Hunter <15943286270@163.com>
2020-10-12 17:13:09 +0800

    前端代码以静态资源的形式整合到web项目中

--- misModule/com.hunter.simple.daily.web/src/main/java/com/hunter/simple/daily/web/WebApplication.java
+++ misModule/com.hunter.simple.daily.web/src/main/java/com/hunter/simple/daily/web/WebApplication.java
@@ -3,16 +3,19 @@ package com.hunter.simple.daily.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {"com.hunter"})
-@ServletComponentScan
+@EnableFeignClients(basePackages = {"com.hunter"})
 @SpringBootApplication
+@ServletComponentScan
+@EnableEurekaClient
 public class WebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
-        System.out.println("======WebApplication启动完成======");
     }
 
 }
